94426db731e98276d539b0688295c941448133bd
root <root@d3a2e0704417>
2017-06-09 05:10:03 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_FDNsqkyWVi",
+  "homePage" : "Main",
+  "name" : "Auto_FDNsqkyWVi",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
